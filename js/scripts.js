314fff0ceefbdf6a8b17d17a1011691106994870
Kyle Rush <kdr213@gmail.com>
2018-01-28 13:26:32 -0800

    Working on masonry layout for github page

--- js/scripts.js
+++ js/scripts.js
@@ -1919,3 +1919,46 @@ mr = (function (mr, $, window, document){
 	  return mr;
 
 }(mr, jQuery, window, document));
+
+function onLoadContainer() {
+  mr.documentReady();
+
+ 
+
+
+};
+var cache = new Object();
+//{'':$('<div class="item">').appendTo("#content-container").load('home.html')};
+
+/** Hash-change, simple cache function */
+$(function() {
+    //debugger;
+    //cache[''] = $('<div class="item">').appendTo("#content-container").load('home.html');
+    $(window).hashchange(function() {
+
+      //alert("hashchange");
+
+      var url = window.location.hash.replace('#', '');
+      if(url == "") {
+        url = "home.html";
+      }
+      
+      $("#content-container").children(":visible").hide();
+      
+      if ( cache[url] ) {  
+        //debugger;
+        cache[url].show();
+      } else {
+          cache[url] = $('<div class="item">').appendTo("#content-container").load(url, function() {
+          $("#content-container").ready(function() {
+            onLoadContainer();
+          });
+
+        });
+
+      }
+    });
+
+    $(window).hashchange();
+
+});
