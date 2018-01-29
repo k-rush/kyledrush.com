15c5bbd08d4800440a4747308ad0deeacfcdad60
Kyle Rush <kdr213@gmail.com>
2018-01-28 18:14:25 -0800

    some js fixes on front-end.  front-end revamp completed and deployed

--- js/scripts.js
+++ js/scripts.js
@@ -1036,75 +1036,77 @@ mr = (function (mr, $, window, document){
     mr.navigation.bar = {};
 
     var documentReady = function($){
-        
-        mr.navigation.nav.element = $('nav');
-        mr.navigation.bar.element = $('nav .nav-bar');
-        
-        // Check for nav element and set outerHeight variable
-        if(mr.navigation.nav.element.length){
-            mr.navigation.nav.outerHeight = mr.navigation.nav.element.outerHeight();
-        }else{
-            mr.navigation.nav.outerHeight = 0;
-        }
-        // Check for a bar type nav
-        if(mr.navigation.bar.element.length){
-            mr.navigation.bar.init();
-        }
-
-        //////////////// Mobile Menu Toggle
-        $('.nav-mobile-toggle').each(function() {
-            bindOnce($(this), function() {
-                $('nav').toggleClass('nav-open');
-            });
-        });
-        //$('.nav-mobile-toggle').on('click', function(){
-        //    $('nav').toggleClass('nav-open');
-        //});
-        
-        $('.menu li').on('click', function(ev){
-            var navItem = $(this),
-                e       = ev || window.event;
+        if(!mr.navDocReady) {
+            mr.navDocReady = true;
             
-            e.stopPropagation();
-            if (navItem.find('ul').length) {
-                navItem.toggleClass('active');
-            } else {
-                navItem.parents('.active').removeClass('active');
+            mr.navigation.nav.element = $('nav');
+            mr.navigation.bar.element = $('nav .nav-bar');
+            
+            // Check for nav element and set outerHeight variable
+            if(mr.navigation.nav.element.length){
+                mr.navigation.nav.outerHeight = mr.navigation.nav.element.outerHeight();
+            }else{
+                mr.navigation.nav.outerHeight = 0;
             }
-        });
-        
-        //////////////// Mobile Menu Applets
-        
-        $('.module-applet').on('click', function(){
-            $(this).toggleClass('active');
-        });
-        
-        $('.module-applet__body').each(function(){
-            var moduleBody = $(this);
-            var farRight = moduleBody.offset().left + moduleBody.outerWidth();
-            if(farRight > $(window).width()){
-                moduleBody.addClass('pos-right');
+            // Check for a bar type nav
+            if(mr.navigation.bar.element.length){
+                mr.navigation.bar.init();
             }
-        });
-        
-        //////////////// Menu dropdown positioning
 
-        $('.menu > li > ul').each(function() {
-            var $window          = $(window);
-            var dropDown         = $(this);
-            var menu             = dropDown.offset();
-            var farRight         = menu.left + dropDown.outerWidth(true);
-            var windowWidth      = $window.width();
-            var multiColumn      = dropDown.hasClass('multi-column');
+            //////////////// Mobile Menu Toggle
+            $('.nav-mobile-toggle').each(function() {
+                bindOnce($(this), function() {
+                    $('nav').toggleClass('nav-open');
+                });
+            });
+            //$('.nav-mobile-toggle').on('click', function(){
+            //    $('nav').toggleClass('nav-open');
+            //});
+            
+            $('.menu li').on('click', function(ev){
+                var navItem = $(this),
+                    e       = ev || window.event;
+                
+                e.stopPropagation();
+                if (navItem.find('ul').length) {
+                    navItem.toggleClass('active');
+                } else {
+                    navItem.parents('.active').removeClass('active');
+                }
+            });
+            
+            //////////////// Mobile Menu Applets
+            
+            $('.module-applet').on('click', function(){
+                $(this).toggleClass('active');
+            });
+            
+            $('.module-applet__body').each(function(){
+                var moduleBody = $(this);
+                var farRight = moduleBody.offset().left + moduleBody.outerWidth();
+                if(farRight > $(window).width()){
+                    moduleBody.addClass('pos-right');
+                }
+            });
+            
+            //////////////// Menu dropdown positioning
 
-            if (farRight > windowWidth && !multiColumn) {
-                dropDown.addClass('make-right');
-            } else if (farRight > windowWidth && multiColumn) {
-                var difference = farRight - windowWidth;
-                dropDown.css('margin-left', -(difference));
-            }
-        });
+            $('.menu > li > ul').each(function() {
+                var $window          = $(window);
+                var dropDown         = $(this);
+                var menu             = dropDown.offset();
+                var farRight         = menu.left + dropDown.outerWidth(true);
+                var windowWidth      = $window.width();
+                var multiColumn      = dropDown.hasClass('multi-column');
 
+                if (farRight > windowWidth && !multiColumn) {
+                    dropDown.addClass('make-right');
+                } else if (farRight > windowWidth && multiColumn) {
+                    var difference = farRight - windowWidth;
+                    dropDown.css('margin-left', -(difference));
+                }
+            });
+        }
     };
 
     ///
